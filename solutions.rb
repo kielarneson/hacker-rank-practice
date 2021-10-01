ba708284d623de9309b58a662843d9af4e68916d
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-30 19:36:57 -0500

    Adding solution for Breaking the Records

--- solutions.rb
+++ solutions.rb
@@ -24,3 +24,31 @@ def string_count(strings, queries)
 end
 
 p string_count(strings, queries)
+
+# Breaking the Records
+scores = [10, 5, 20, 20, 4, 5, 2, 25, 1]
+
+def high_score_count(array)
+  high_score = array[0]
+  low_score = array[0]
+  index = 0
+  high_count = 0
+  low_count = 0
+  high_and_low = []
+
+  while index < array.length
+    if array[index] > high_score
+      high_score = array[index]
+      high_count += 1
+    elsif array[index] < low_score
+      low_score = array[index]
+      low_count += 1
+    end
+    index += 1
+  end
+  high_and_low << high_count
+  high_and_low << low_count
+  return high_and_low
+end
+
+p high_score_count(scores)
