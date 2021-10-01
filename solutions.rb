372d11357997ccba15883017531cfa2e16da4274
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-30 20:10:44 -0500

    Commenting out previous solutions

--- solutions.rb
+++ solutions.rb
@@ -1,83 +1,83 @@
-# Sparse Arrays
-strings = ["aba", "baba", "aba", "xzxb"]
-queries = ["aba", "xzxb", "ab"]
+# # Sparse Arrays
+# strings = ["aba", "baba", "aba", "xzxb"]
+# queries = ["aba", "xzxb", "ab"]
 
-def string_count(strings, queries)
-  index1 = 0
-  index2 = 0
-  count = 0
-  count_array = []
+# def string_count(strings, queries)
+#   index1 = 0
+#   index2 = 0
+#   count = 0
+#   count_array = []
 
-  while index1 < queries.length
-    while index2 < strings.length
-      if queries[index1] == strings[index2]
-        count += 1
-      end
-      index2 += 1
-    end
-    index1 += 1
-    index2 = 0
-    count_array << count
-    count = 0
-  end
-  return count_array
-end
+#   while index1 < queries.length
+#     while index2 < strings.length
+#       if queries[index1] == strings[index2]
+#         count += 1
+#       end
+#       index2 += 1
+#     end
+#     index1 += 1
+#     index2 = 0
+#     count_array << count
+#     count = 0
+#   end
+#   return count_array
+# end
 
-p string_count(strings, queries)
+# p string_count(strings, queries)
 
-# Breaking the Records
-scores = [10, 5, 20, 20, 4, 5, 2, 25, 1]
+# # Breaking the Records
+# scores = [10, 5, 20, 20, 4, 5, 2, 25, 1]
 
-def high_score_count(array)
-  high_score = array[0]
-  low_score = array[0]
-  index = 0
-  high_count = 0
-  low_count = 0
-  high_and_low = []
+# def high_score_count(array)
+#   high_score = array[0]
+#   low_score = array[0]
+#   index = 0
+#   high_count = 0
+#   low_count = 0
+#   high_and_low = []
 
-  while index < array.length
-    if array[index] > high_score
-      high_score = array[index]
-      high_count += 1
-    elsif array[index] < low_score
-      low_score = array[index]
-      low_count += 1
-    end
-    index += 1
-  end
-  high_and_low << high_count
-  high_and_low << low_count
-  return high_and_low
-end
+#   while index < array.length
+#     if array[index] > high_score
+#       high_score = array[index]
+#       high_count += 1
+#     elsif array[index] < low_score
+#       low_score = array[index]
+#       low_count += 1
+#     end
+#     index += 1
+#   end
+#   high_and_low << high_count
+#   high_and_low << low_count
+#   return high_and_low
+# end
 
-p high_score_count(scores)
+# p high_score_count(scores)
 
-# Plus Minus
-numbers = [-4, 3, -9, 0, 4, 1]
+# # Plus Minus
+# numbers = [-4, 3, -9, 0, 4, 1]
 
-def ratios(array)
-  index = 0
-  positive_count = 0
-  negative_count = 0
-  zero_count = 0
+# def ratios(array)
+#   index = 0
+#   positive_count = 0
+#   negative_count = 0
+#   zero_count = 0
 
-  while index < array.length
-    if array[index] > 0
-      positive_count += 1
-    elsif array[index] < 0
-      negative_count += 1
-    elsif array[index] == 0
-      zero_count += 1
-    end
-    index += 1
-  end
-  p (positive_count.to_f / array.length)
-  p (negative_count.to_f / array.length).round(6)
-  p (zero_count.to_f / array.length).round(6)
-end
+#   while index < array.length
+#     if array[index] > 0
+#       positive_count += 1
+#     elsif array[index] < 0
+#       negative_count += 1
+#     elsif array[index] == 0
+#       zero_count += 1
+#     end
+#     index += 1
+#   end
+#   p (positive_count.to_f / array.length)
+#   p (negative_count.to_f / array.length).round(6)
+#   p (zero_count.to_f / array.length).round(6)
+# end
 
-ratios(numbers)
+# ratios(numbers)
 
 # Mini-Max Sum
 numbers = [1, 2, 3, 4, 5]
